19325fd1a933524412ba6ef41a3deb5a9a71c34f
Camilla Sonesson <Camilla_Sonesson@hotmail.com>
2023-03-10 06:02:50 +0000

    added code for admin page

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Contact
 
 # Register your models here.
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('fname', 'sname', 'email', 'subject', 'created_on')
+    search_fields = ('fname', 'sname', 'email', 'subject', 'body')
+    list_filter = ('fname', 'sname', 'email')
